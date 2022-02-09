ec5f89289186ba86ced5d68173924355e19f69dd
Roylu7384 <roylu7384@gmail.com>
2022-02-09 15:00:03 -0500

    complete findKeyByValue function assignment

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+// copy pasted assertEqual function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// actual function
+const findKeyByValue = function(obj, str) {
+  for (const key in obj) {
+    if (str === obj[key]) return key;
+  }
+};
+
+
+// TEST CODE
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
