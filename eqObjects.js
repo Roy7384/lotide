9a427bab86afad06b508aeb082e25b9cb49a5af0
Roylu7384 <roylu7384@gmail.com>
2022-02-09 15:23:37 -0500

    complete eqObjects function assignment

---
+++ eqObjects.js
@@ -0,0 +1,55 @@
+// copy pasted assertEqual function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// copy pasted eqArrays function
+const eqArrays = function(arr1, arr2) {
+
+  // first check if two arrays have the same amount of elements
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  // compare two arrays element by element
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i  ]) return false;
+  }
+
+  // if both tests above passed, return true
+  return true;
+};
+
+
+
+// Actual function
+const eqObjects = function(object1, object2) {
+  // get the numbers of keys of both objects
+  const obj1Length = Object.keys(object1).length;
+  const obj2Length = Object.keys(object2).length;
+
+  if (obj1Length !== obj2Length) {
+    return false;
+  }
+  for (const key in object1) {
+    if (Array.isArray(object1[key])) { // if the key value pair to compare has array as value
+      return eqArrays(object1[key], object2[key]);
+    }
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// TEST CODE
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
