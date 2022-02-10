a3fa3350c79a7f750f885ba596e99a402370b92b
Roylu7384 <roylu7384@gmail.com>
2022-02-10 08:22:25 -0500

    let countLetters function skip space in a sentence

--- countLetters.js
+++ countLetters.js
@@ -13,6 +13,8 @@ const countLetters = function(str) {
   let result = {};
 
   for (const char of str) {
+
+    if (char === ' ') continue;  // skip space character since we are only counting letters
     if (result[char]) { // check if the key char is added to result or not
       result[char] += 1; // add one count to the already created key value
     } else {
@@ -25,7 +27,7 @@ const countLetters = function(str) {
 
 
 // TEST CODE
-const testStr = 'helloworld';
+const testStr = 'hello world';
 const result = countLetters(testStr);
 
 assertEqual(result['h'], 1);
