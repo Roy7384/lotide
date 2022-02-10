729aa094e275fe9aba4205c17a556aebe2cae482
Roylu7384 <roylu7384@gmail.com>
2022-02-10 14:46:40 -0500

    refactor letterPositions.js to reduce use of else and if nesting

--- letterPositions.js
+++ letterPositions.js
@@ -28,13 +28,12 @@ const letterPositions = function(str) {
   let result = {};
 
   for (let i = 0; i < str.length; i ++) {  // iterate through the string by characters
-    if (str[i] !== ' ') {  // skip spaces
-      if (result[str[i]]) {  // if array of positions already exist, add the position to the array
-        result[str[i]].push(i);
-      } else {
-        result[str[i]] = [i];   // initilize the key value pair
-      }
+    if (str[i] === ' ') continue;   // skip spaces
+    if (result[str[i]])    {  // if array of positions already exist, add the position to the array
+      result[str[i]].push(i);
+      continue;
     }
+    result[str[i]] = [i];   // otherwise initilize the key value pair
   }
 
   return result;
