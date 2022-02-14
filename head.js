b7d144d0d3dd47d5e0cad8d1f87154211ad64fd4
Roylu7384 <roylu7384@gmail.com>
2022-02-14 14:03:43 -0500

    refactor head.js and assertEqual.js to be modular

--- head.js
+++ head.js
@@ -1,12 +1,3 @@
-// function copied from assertEqual.js
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 
 // actual head function
 const head = function(arr) {
@@ -14,8 +5,4 @@ const head = function(arr) {
 };
 
 
-// TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([2]), 2);
-assertEqual(head([]), undefined);
+module.exports = head;
